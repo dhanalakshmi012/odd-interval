51dde5b587db011a8219843565adc93d536dc3a5
dhanalakshmi012 <33174588+dhanalakshmi012@users.noreply.github.com>
2018-01-04 10:19:54 +0530

    Create odd num.py

---
+++ odd num.py
@@ -0,0 +1,9 @@
+# odd-interval
+upper=raw_input("enter the num:")
+lower=raw_input("enter the num:")
+num=raw_input("enter the num:")
+for num in range(lower,upper+1)
+   if(num%2)==0:
+      print("even")
+   else:
+      print("odd")
