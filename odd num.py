cb4b55686471819aff3de9ce63a37d9ae77a5cfe
dhanalakshmi012 <33174588+dhanalakshmi012@users.noreply.github.com>
2018-01-04 10:25:34 +0530

    Update odd num.py

--- odd num.py
+++ odd num.py
@@ -2,8 +2,6 @@
 upper=raw_input("enter the num:")
 lower=raw_input("enter the num:")
 num=raw_input("enter the num:")
-for num in range(lower,upper+1)
-   if(num%2)==0:
-      print("even")
-   else:
-      print("odd")
+for num in range(lower,upper+1):
+   (num%2)!=0
+   print(i)
